0744688338110b826f94bfb8c25c645068375d8f
Hruzduk <hruzduchka@gmail.com>
2020-10-25 15:56:35 +0200

    # добавлено 16 - 17

---
+++ lesson_01/home_work/chapter_3/task_z_18.py
@@ -0,0 +1,21 @@
+central_pizza = str('"Центральная пицерия"')
+coffee = str('"Кофе за углом"')
+italian = str('"Блюда от итальянской мамы"')
+kitchen = str('"Кухня шеф-повара"')
+
+print("Пожалуйста, ответье будут ли на ужине (да / нет):  ")
+vegetarians = str(input("Вегетарианци?  "))
+vegans = str(input("Веганы?  "))
+out_gluten = str(input("Приверженци безглютеновой диеты? "))
+
+if vegetarians == str("нет") and vegans == str("нет") and out_gluten == str("нет"):
+    print(f'{central_pizza}, {coffee}, {italian}, {kitchen} и "Изысканные гамбургеры от Джо"')
+else:
+    if vegetarians == str("да") and vegans == str("да") and out_gluten == str("да"):
+        print(f'{coffee} "и" {kitchen}')
+        if vegetarians == str("да") and vegans == str("нет") and out_gluten == str("нет"):
+            print(f'{italian}')
+            if vegetarians == str("да") and vegans == str("нет") and out_gluten == str("да"):
+            print(f'{central_pizza}')
+
+# изрядно высушило мозг
